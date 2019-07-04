e06a6ae92f24486f187a6e59f12f0d411e0a92bc
lylyonl <vumhung1997@gmail.com>
2019-07-04 10:01:00 +0700

    init_base_project

---
+++ app/src/main/java/com/sun_asterisk/myeditor03/screen/home/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.sun_asterisk.myeditor03.screen.home
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.sun_asterisk.myeditor03.R.layout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_main)
+    }
+}
