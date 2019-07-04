6586394bae1968447dc2d9db334d1917de13fd08
lylyonl <vumhung1997@gmail.com>
2019-07-04 13:46:58 +0700

    View title  photo and collection

---
+++ app/src/main/java/com/sun_asterisk/myeditor03/utils/CommomUtils.kt
@@ -0,0 +1,10 @@
+package com.sun_asterisk.myeditor03.utils
+
+class CommomUtils {
+    interface Genres {
+        companion object {
+            const val PHOTO = "Photos"
+            const val COLLECTION = "Collections"
+        }
+    }
+}
