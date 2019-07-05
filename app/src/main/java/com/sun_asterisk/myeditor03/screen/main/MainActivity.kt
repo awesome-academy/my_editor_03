ae59baf649b36ae1e0ea2a3312097396fbe37c76
lylyonl <vumhung1997@gmail.com>
2019-07-05 13:47:58 +0700

    fix commented 3

--- app/src/main/java/com/sun_asterisk/myeditor03/screen/main/MainActivity.kt
+++ app/src/main/java/com/sun_asterisk/myeditor03/screen/main/MainActivity.kt
@@ -5,14 +5,14 @@ import android.support.v7.app.AppCompatActivity
 import com.sun_asterisk.myeditor03.R
 import com.sun_asterisk.myeditor03.R.layout
 import com.sun_asterisk.myeditor03.screen.home.HomeFragment
-import com.sun_asterisk.myeditor03.utils.goToFragment
+import com.sun_asterisk.myeditor03.utils.addFragmentToActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_main)
-        goToFragment(
+        addFragmentToActivity(
             R.id.layoutContainer,
             HomeFragment.instance())
     }
--- app/src/main/java/com/sun_asterisk/myeditor03/utils/ActivityExtention.kt
+++ app/src/main/java/com/sun_asterisk/myeditor03/utils/ActivityExtention.kt
@@ -5,7 +5,7 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
 
-fun AppCompatActivity.goToFragment(
+fun AppCompatActivity.addFragmentToActivity(
     @IdRes containerViewId: Int,
     fragment: Fragment, tag: String = fragment::class.java.simpleName, addToBackStack: Boolean = false
 ) {
